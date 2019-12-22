6d2b523f4273199d61ed13dc03bca7c8fc696185
shinnosuke.harigaya <shinnosuke.harigaya@gmail.com>
2019-12-22 12:43:16 +0900

    init

---
+++ myApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Shinnosuke Harigaya on 2019/12/22.
+//  Copyright © 2019 Shinnosuke Harigaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
