bf8baa768c8093a9b1bb8120696fbc95e5c8b9de
shinnosuke.harigaya <shinnosuke.harigaya@gmail.com>
2019-12-28 12:02:08 +0900

    コメントの追加

--- myApp/ViewController.swift
+++ myApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // 差分
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
