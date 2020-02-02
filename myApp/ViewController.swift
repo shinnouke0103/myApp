b3b3b7bbf36c794bc6c023741cc029e9fef40695
shinnosuke.harigaya <shinnosuke.harigaya@gmail.com>
2020-02-02 11:28:34 +0900

    アラートの追加、キーボードのエラーの解消、数字のキーボードにした

--- myApp/ViewController.swift
+++ myApp/ViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // 差分
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
     @IBOutlet var plusLabel: UILabel!
     @IBOutlet var minusLabel: UILabel!
     @IBOutlet var multLabel: UILabel!
@@ -24,9 +17,6 @@ class ViewController: UIViewController {
     @IBOutlet var ruijouLabel: UILabel!
     @IBOutlet var rootLabel: UILabel!
     @IBOutlet var totalLabel: UILabel!
-    @IBOutlet var pLabel: UILabel!
-    @IBOutlet var cLabel: UILabel!
-    @IBOutlet var noudoLabel: UILabel!
     @IBOutlet var maruTextField: UITextField!
     @IBOutlet var shikakuTextField: UITextField!
     
@@ -40,78 +30,54 @@ class ViewController: UIViewController {
     var ruijouans: Double = 0
     var rootans: Double = 0
     var totalans: Int = 0
-    var pans: Int = 0
-    var cans: Int = 0
-    var noudoans: Double = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        shikakuTextField.keyboardType = .numberPad
+        maruTextField.keyboardType = .numberPad
+    }
+    
+    
     
     
     @IBAction func calculate() {
-        
-        if shikakuTextField.text != "" && maruTextField.text != "" {
-            
-            maru = Int(maruTextField!.text!)!
-            shikaku = Int(shikakuTextField!.text!)!
-            
-            plusans = maru + shikaku
-            plusLabel.text = String(plusans)
-            
-            minusans = maru - shikaku
-            minusLabel.text = String(minusans)
-            
-            multans = maru * shikaku
-            multLabel.text = String(multans)
-            
-            divans = Double(maru) / Double(shikaku)
-            divLabel.text = String(divans)
-            
-            ruijouans = pow(Double(maru),Double(shikaku))
-            ruijouLabel.text = String(ruijouans)
-            
-            rootans = pow(Double(maru), 1 / Double(shikaku))
-            rootLabel.text = String(rootans)
-            
-            if maru < shikaku{
-                for i in maru...shikaku {
-                    totalans += i
+        if let shikakuText = shikakuTextField.text, let maruText = maruTextField.text {
+            if let maru = Int(maruText), let shikaku = Int(shikakuText) {
+                plusans = maru + shikaku
+                plusLabel.text = String(plusans)
+                
+                minusans = maru - shikaku
+                minusLabel.text = String(minusans)
+                
+                multans = maru * shikaku
+                multLabel.text = String(multans)
+                
+                divans = Double(maru) / Double(shikaku)
+                divLabel.text = String(divans)
+                
+                ruijouans = pow(Double(maru),Double(shikaku))
+                ruijouLabel.text = String(ruijouans)
+                
+                rootans = pow(Double(maru), 1 / Double(shikaku))
+                rootLabel.text = String(rootans)
+                
+                if maru < shikaku{
+                    for i in maru...shikaku {
+                        totalans += i
+                    }
                 }
+                totalLabel.text = String(totalans)
+            } else {
+                
+                showAlert()
+                
             }
             
-            
-            totalLabel.text = String(totalans)
-            
-            if maru < shikaku {
-                for i in maru...shikaku {
-                    pans *= (i-1)
-                }
-            }
-            
-            pLabel.text = String(pans)
-            
-            cans = pans
-            for i in 1...shikaku {
-                cans /= i
-            }
-            
-            cLabel.text = String(cans)
-            
-            noudoans = Double(shikaku / maru * 100)
-            
-            
-            noudoLabel.text = String("\(noudoans)%")
             
         } else {
             
-            // アラートを出す
-            let alert = UIAlertController(
-                title: "emergency",
-                message: "数値を入力してください",
-                preferredStyle: .alert
-            )
-            alert.addAction(UIAlertAction(
-                title: "了解",
-                style: .default,
-                handler: nil
-            ))
+            showAlert()
             
         }
         
@@ -123,4 +89,21 @@ class ViewController: UIViewController {
         shikakuTextField.text = ""
         maruTextField.text = ""
     }
+    
+    func showAlert() {
+        
+        let alert = UIAlertController(
+            title: "emergency",
+            message: "数値を入力してください",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(
+            title: "了解",
+            style: .default,
+            handler: nil
+        ))
+        present(alert, animated: true, completion: nil)
+        
+    }
+        
 }
